e4cb3c6e964acffc65312f7035e2f4b042df973e
wiktorpp <grazynapp65@gmail.com>
2021-05-01 12:04:39 +0200

    Added compression option

--- file_to_qr.py
+++ file_to_qr.py
@@ -4,14 +4,18 @@
 # "self.data = content", 
 # else the program will error out
 
+compress = True
+
 import sys
 import bz2
 import pyqrcode
 
 data = open(sys.argv[1], "rb").read()
+if compress:
+    data = bz2.compress(data)
 n = 1003
 chunks = [data[i:i+n] for i in range(0, len(data), n)]
 for i, chunk in enumerate(chunks):
     img = pyqrcode.create(chunk, error='L', mode='binary')
     #img.show()
-    img.png(f"{sys.argv[1]}.pt{str(i)}.png")
+    img.png(f"{sys.argv[1]}{'.bz2' if compress else ''}.pt{str(i)}.png")
